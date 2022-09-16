a7edbb5ee76f85f19f91b099b1d9d9055c243e6e
srinivaskoushik <srinivas.koushik4@gmail.com>
2022-09-16 01:14:11 -0400

    Bitcoin Simulator using Actor Model:removed files

--- Bitcoin Simulator/src/run.sh
+++
@@ -1,6 +0,0 @@
-erl -sname boss.
-net_adm:ping('worker1@Srinivass-MacBook-Air').
-Names = ["kondubhatlas;fnew1oer","kondubhatlas;wfmlw2324","kondubhatlas;1SFEF23enlwf","kondubhatlas;1fds3e3qe","kondubhatlas;fenwjklfnw","kondubhatlas;2324fweJFDfrs","kondubhatlas;fdsjlfnse12","kondubhatlas;13edw3DF","kondubhatlas;fdsnjkfs21","kondubhatlas;mkifsjfodws1213", "kondubhatlas;131FNMLKwewq","kondubhatlas;1pfhnsdASE45"].
-Ks = [4,4,5,6,6,2,3,6,5,2,3,4].
-c(mining).
-mining:main(Names,Ks,0,1,0).
